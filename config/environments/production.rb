117ea031d7bd6d34e1cd5db3236115fef350c35a
supritkumar shah <suprit7@gmail.com>
2016-08-05 16:18:35 -0400

    Fix action cable for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ config.assets.digest = true
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://evening-sierra-60192.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://evening-sierra-60192.herokuapp.com', /http:\/\/evening-sierra-60192.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
